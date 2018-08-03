8102ccd152d62c8058ae88112cb72d0d5b3e0471
unknown <kaden.zipfel@hotmail.com>
2018-08-02 18:58:56 -0700

    Add basic routes and views & bootstrap

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// ======
+// ROUTES
+// ======
+
+// Index Route
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Portfolio Route
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio');
+});
+
+// Contact Route
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
 app.listen(PORT, () => {
