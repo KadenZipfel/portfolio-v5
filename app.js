d30082f52b94c9f84fbca15836bc02f83e5ee88c
unknown <kaden.zipfel@hotmail.com>
2018-08-12 18:10:08 -0700

    Add basic styles

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -6,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // ======
 // ROUTES
@@ -24,6 +28,37 @@ app.get('/portfolio', (req, res) => {
 // Contact Route
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+// Contact Post Route
+app.post('/contact', (req, res) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: keys.google.user,
+      pass: keys.google.pass
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Contact Form" <kaden.zipfel@hotmail.com>', // sender address
+    to: 'kaden.zipfel@hotmail.com', // list of receivers
+    subject: req.body.subject + ' - ' + req.body.email, // Subject line
+    text: req.body.message, // plain text body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent');
+  });
+  res.redirect('back');
 });
 
 // About Route
